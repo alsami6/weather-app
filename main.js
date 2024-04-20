9536397ad9c6b7457757a0b8baf681adfa3a236d
Al <alsami1209@gmail.com>
2024-04-20 17:57:59 +0600

    done

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const apiKey = "a80df3130815ee475806a399964116f3";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
 
-async function checkWeather(){
-    const res = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city){
+    const res = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await res.json();
 
     console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
 }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
 
 checkWeather()
